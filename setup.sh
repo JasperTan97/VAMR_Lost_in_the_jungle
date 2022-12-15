c1431e18fc2992432bf88d66e42c5eb597ac3343
Tianyi Lim <tian.lim19@imperial.ac.uk>
2022-12-15 21:25:23 +0100

    some further updates...

--- setup.sh
+++ setup.sh
@@ -6,16 +6,16 @@ conda create -n vamr_proj python==3.10
 conda activate vamr_proj
 pip3 install --user -r requirements.txt
 
-# Download datasets
+# Download and unzip datasets. "&" does it in parallel.
 cd data
-wget https://rpg.ifi.uzh.ch/docs/teaching/2022/parking.zip
-wget https://rpg.ifi.uzh.ch/docs/teaching/2022/kitti05.zip
-wget https://rpg.ifi.uzh.ch/docs/teaching/2022/malaga-urban-dataset-extract-07.zip
+wget https://rpg.ifi.uzh.ch/docs/teaching/2022/parking.zip && \
+    unzip -a parking.zip &
 
-# unzip datasets
-unzip -a parking.zip
-unzip -a kitti05.zip
-unzip -a malaga-urban-dataset-extract-07.zip
+wget https://rpg.ifi.uzh.ch/docs/teaching/2022/kitti05.zip && \
+    unzip -a kitti05.zip &
+
+wget https://rpg.ifi.uzh.ch/docs/teaching/2022/malaga-urban-dataset-extract-07.zip && \
+    unzip -a malaga-urban-dataset-extract-07.zip
 
 # return to the original directory.
 cd ..
